a53ad82cbde2dbfe54c655754a4dd9b61512f0fa
Lina <linazon94@gmail.comgit>
2021-05-24 22:47:53 -0500

    se crean las variables en lo que seria mi constructor

---
+++ pong/main.js
@@ -0,0 +1,16 @@
+//creo mi tablero 
+(function(){
+    var self.Board = function(width,height){
+        this.width = width;
+        this.height = height; //tamaño tablero
+        this.playing = false; // var bool, si esta jugando
+        this.game_over = false; // var bool, si perdio
+    }
+
+    self.Board.prototype ={
+        
+    }
+})();
+function main(){
+
+}
