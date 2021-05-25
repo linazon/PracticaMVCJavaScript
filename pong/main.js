6776d7679568cdad452f2e7c1ba2511f6d70356c
Lina <linazon94@gmail.comgit>
2021-05-24 23:00:15 -0500

    se generan las variables que se van a usar

--- pong/main.js
+++ pong/main.js
@@ -5,12 +5,24 @@
         this.height = height; //tamaño tablero
         this.playing = false; // var bool, si esta jugando
         this.game_over = false; // var bool, si perdio
+        this.bars = [];
+        this.ball = null;
     }
 
+    // modificar el prototipo para agregar los metodos
     self.Board.prototype ={
-        
+        //agregar objeto json
+        get elements(){
+            // me trae los elementos (bars)
+            var elements = this.bars; // barras laterales del juego
+            elements.push(ball);
+            return elements;
+        }
     }
 })();
+
+// clase para dibujar el tablero
+
 function main(){
 
 }
