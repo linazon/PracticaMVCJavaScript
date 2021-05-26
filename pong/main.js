b9383913c9943e3702312163cc24488d2410bc81
Lina <linazon94@gmail.comgit>
2021-05-26 15:56:56 -0500

    se termina el video 4: se crea la clase pelota y se dibuja la pelota

--- pong/main.js
+++ pong/main.js
@@ -15,9 +15,24 @@
         get elements(){
             // me trae los elementos (bars)
             var elements = this.bars; // barras laterales del juego
-            //elements.push(this.ball);
+            elements.push(this.ball);
             return elements;
         }
+    }
+})();
+
+//clase pelota
+(function(){
+    self.Ball = function(x,y,radius,board){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+
+        board.ball = this;
+        this.kind = "circle";
     }
 })();
 
@@ -70,15 +85,25 @@
                 var el = this.board.elements[i];
                 draw(this.ctx,el);
             };
+        },
+        play: function(){
+           this.clean();
+           this.draw();  
         }
     }
 
-    //dibuja las barras en el tablero
+    //dibuja las barras y la pelota en el tablero
     function draw(ctx,element){
         //if(element !== null && element.hasOwnProperty("kind")){
             switch(element.kind){
                 case "rectangle":
                     ctx.fillRect(element.x, element.y, element.width, element.height);
+                break;
+                case "circle":
+                    ctx.beginPath();
+                    ctx.arc(element.x,element.y,element.radius,0,7);
+                    ctx.fill();
+                    ctx.closePath();
                 break;
             }
         //}
@@ -93,6 +118,7 @@ var limite = new Bar(50,10,700,10,board);
 var limite = new Bar(50,380,700,10,board);
 var canvas = document.getElementById('canvas');
 var board_View = new BoardView(canvas,board);
+var ball = new Ball(350,150,10,board);
 
 //ejecuto las funciones up y down, según se oprima las teclas
 document.addEventListener("keydown",function(ev){
@@ -119,7 +145,8 @@ document.addEventListener("keydown",function(ev){
 window.requestAnimationFrame(controller);
 
 function controller(){
-    board_View.clean();
-    board_View.draw();
+    //board_View.clean();
+    //board_View.draw();
+    board_View.play();
     window.requestAnimationFrame(controller);
 }
